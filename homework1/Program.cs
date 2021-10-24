2043788005574380c6b61bb8bcf575c032f1fb91
Gulay Behramova <gulaybehramova@gmail.com>
2021-10-24 19:27:34 +0400

    Visual Studio

---
+++ homework1/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace homework1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*1.*/
+            //int[] numbers = { 12, 23, 67, 89, 45 };
+            //int counter = 0;
+
+
+            //    for (int i = 0; i < numbers.Length; i++)
+            //{
+            //   var num = numbers [i];
+            //    var sum = 0;
+            //    while (num > 0)
+
+            //    {
+            //        var lastdigit = num % 10;
+            //        sum += lastdigit;
+            //        num = (num - lastdigit) / 10;
+            //    }
+            //    if (sum < 10)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //    if(counter > 0)
+            //{
+            //    Console.WriteLine(counter);
+            //}
+            //else
+            //{
+            //    Console.WriteLine(" siyahida reqemlerinin cemi 10-dan kicik olan element yoxdur");
+            //}
+
+
+
+            /*2.*/
+            //int counter = 0;
+            //for (int i = 0; i < 8000; i++)
+            //{
+            //    if (i % 2 == 0)
+            //    {
+            //        counter++;
+            //    }
+            //}
+            //Console.WriteLine("1den 8000dek ededler icerisinde " + counter + " sayda cut eded var");
+
+
+            
+           /*3.*/
+           //int n = Convert.ToInt32(Console.ReadLine());
+            //int result;
+            //int i = 1;
+            //while(i * i <= n)
+            //{
+            //    i++;
+            //}
+            //result = i - 1;
+            //Console.WriteLine(n+ "ededin koku = " + result);
+        }
+    }
+}
